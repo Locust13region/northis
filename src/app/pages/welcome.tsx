4a094bc25920c2a215afef153e56fc4f7f3fc211
DESKTOP-MF8V5D1\Andrey <m>
2024-08-20 15:42:42 +0300

    update about on title page

--- src/app/pages/welcome.tsx
+++ src/app/pages/welcome.tsx
@@ -6,6 +6,7 @@ export default function Welcome() {
 		<Box
 			flexGrow={1}
 			display={"flex"}
+			flexDirection={"column"}
 			justifyContent={"center"}
 			alignItems={"center"}
 		>
@@ -16,6 +17,29 @@ export default function Welcome() {
 				color={"deepGray.main"}
 			>
 				Добро пожаловать
+			</Typography>
+			<Typography
+				width={0.5}
+				paragraph
+				fontSize={"16px"}
+				fontWeight={400}
+				color={"deepGray.main"}
+			>
+				в Web-приложение для поиска репозиториев GitHub с помощью GitHub REST
+				API. Результаты поиска представлены в виде таблицы со следующими
+				столбцами: название, язык, число форков, число звёзд, дата обновления.
+			</Typography>
+			<Typography
+				width={0.5}
+				paragraph
+				fontSize={"16px"}
+				fontWeight={400}
+				color={"deepGray.main"}
+			>
+				В таблице реализована пагинация и сортировка с возможностью выбора
+				направления по следующим столбцам: число звёзд, число форков, дата
+				обновления. По выбору строки таблицы отображаются детали: название,
+				описание, лицензия.
 			</Typography>
 		</Box>
 	);
